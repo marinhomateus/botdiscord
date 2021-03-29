4ab17636784add9d6194f921dc10b901778990a6
Mateus Marinho <mateus.marinho@hotmail.com.br>
2021-03-29 12:39:04 -0300

    bot login

---
+++ index.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js'); //importando o arquivo com as funcionalidades do discord
+
+const bot = new Discord.Client(); //instanciamos uma classe
+
+const token = 'ODI2MTE1MDg4ODc1NjUxMTEy.YGHxoQ.OEeeFNKyV4DOiLlhGzOUl3SHku4';
+
+bot.login(token);
