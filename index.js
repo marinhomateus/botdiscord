dc678400682ca5731fc1fd9e278da8f16c694cf2
Mateus Marinho <mateus.marinho@hotmail.com.br>
2021-03-31 12:11:35 -0300

    comandos

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ client.on("message", async (message) => {
 
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const comando = args.shift().toLowerCase();
+
+    if(comando === "your_command") //for the personalized command you can use this pattern
+        client.player.play(message, 'link_you_want_to_execute');
+
+    if(comando === "get you") //Example     ?get you        will play the linked video song
+        client.player.play(message, 'https://www.youtube.com/watch?v=uQFVqltOXRg');
+
+    if(comando === "play")
+        client.player.play(message, args[0]);
+
+    //EXAMPLE           ?play Sacrifice         Will play the first 'Sacrifice' result found on youtube
+
+}
+
+client.login(config.token);//log the bot by a token
