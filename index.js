60ad1067a6417cc19ed3eeca2ace1f33191e4777
Mateus Marinho <mateus.marinho@hotmail.com.br>
2021-06-02 17:28:48 -0300

    align items

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const config = require("./config.json"); 
+const config = require("./config.json");
 const Discord = require("discord.js"),
-client = new Discord.Client();
+  client = new Discord.Client();
 
 const { Player } = require("discord-player");
 const player = new Player(client);
 client.player = player;
 
 client.on("ready", () => {
-    console.log("Bot em funcionamento!");//mensagem de ativaçao do bot
+  console.log("Bot em funcionamento!"); //mensagem de ativaçao do bot
 });
 
 client.on("message", async (message) => {
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const comando = args.shift().toLowerCase();
 
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const comando = args.shift().toLowerCase();
+  if (comando === "your_command")
+    //for the personalized command you can use this pattern
+    client.player.play(message, "link_you_want_to_execute");
 
-    if(comando === "your_command") //for the personalized command you can use this pattern
-        client.player.play(message, 'link_you_want_to_execute');
+  if (comando === "get_you")
+    //Example     ?get you        will play the linked video song
+    client.player.play(message, "https://www.youtube.com/watch?v=uQFVqltOXRg");
 
-    if(comando === "get_you") //Example     ?get you        will play the linked video song
-        client.player.play(message, 'https://www.youtube.com/watch?v=uQFVqltOXRg');
+  if (comando === "play") client.player.play(message, args[0]);
 
-    if(comando === "play")
-        client.player.play(message, args[0]);
-
-    //EXAMPLE           ?play Sacrifice         Will play the first 'Sacrifice' result found on youtube
-
+  //EXAMPLE           ?play Sacrifice         Will play the first 'Sacrifice' result found on youtube
 });
 
-client.login(config.token);//log the bot by a token
+client.login(config.token); //log the bot by a token
